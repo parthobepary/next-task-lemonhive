55c6c6d582b480202fa179f4ca1edc9b8d30c4a5
pallobbiswas <pallobbiswas326@gmail.com>
2022-10-31 14:02:31 +0600

    done

--- components/landingPage/TableBody.js
+++ components/landingPage/TableBody.js
@@ -5,7 +5,15 @@ import style from "../../styles/TableBody.module.css";
 function TableBody({ allevents }) {
   const details = (item) => {
     console.log(item);
+  };
+
+  const eventHandler = (e, data) => {
+    console.log('Event Type', e.type);
+    console.log({e, data});
   }
+ 
+
+
   return (
     <div className="flex">
       <div className={`${style.slot_time}`}>
@@ -24,13 +32,18 @@ function TableBody({ allevents }) {
                       className={`flex items-center justify-center h-full ${style.slot_time}`}
                       key={i}
                     >
-                      <Draggable>
+                      <Draggable onStart={eventHandler}>
                         <div className={style.drag_box}>
                           <p className={style.drag_hading}>{ev.time}</p>
                           <div>
                             <p>{ev.name}</p>
                             <p>{ev.distance}</p>
-                            <button onClick={()=>details(ev)} className="btn btn-xs">details</button>
+                            <button
+                              onClick={() => details(ev)}
+                              className="btn btn-xs"
+                            >
+                              details
+                            </button>
                           </div>
                         </div>
                       </Draggable>
